bb603df54fd9d15ab119e33ac105f889a688f1d5
ucupyh <hadiyus321@gmail.com>
2020-03-12 16:07:22 +0700

    Update register

--- src/route.js
+++ src/route.js
@@ -1,63 +1,63 @@
-import {createAppContainer,createSwitchNavigator} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-import Login from './screen/login'
-import Register from './screen/register'
-import Home from './screen/home'
-import Tambah from './screen/tambahlabel'
-import Daftar from './screen/daftarlabel'
-import Detail from './screen/detaillabel'
-import ImageCarousel from './ImageCarousel'
+import Login from './screen/login';
+import Register from './screen/register';
+import Home from './screen/home';
+import Tambah from './screen/tambahlabel';
+import Daftar from './screen/daftarlabel';
+import Detail from './screen/detaillabel';
+import ImageCarousel from './ImageCarousel';
 
 const AuthStack = createStackNavigator({
-    Login: {
-        screen: Login,
-        navigationOptions: {
-            header: null
-        }
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      headerShown: false,
     },
-    Register: {
-        screen: Register,
-        navigationOptions: {
-            header: null
-        }
-    }
+  },
+  Register: {
+    screen: Register,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
 });
 const Route = createStackNavigator({
-    Home: {
-        screen: Home,
-        navigationOptions: {
-            headerShown: false
-        }
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      headerShown: false,
     },
-    Tambah: {
-        screen: Tambah,
-        navigationOptions: {
-            headerShown: false
-        }
+  },
+  Tambah: {
+    screen: Tambah,
+    navigationOptions: {
+      headerShown: false,
     },
-    ImageCarousel: {
-        screen: ImageCarousel,
-        navigationOptions: {
-            headerShown: false
-        }
+  },
+  ImageCarousel: {
+    screen: ImageCarousel,
+    navigationOptions: {
+      headerShown: false,
     },
-    Daftar: {
-        screen: Daftar,
-        navigationOptions: {
-            headerShown: false
-        }
+  },
+  Daftar: {
+    screen: Daftar,
+    navigationOptions: {
+      headerShown: false,
     },
-    Detail: {
-        screen: Detail,
-        navigationOptions: {
-            headerShown: false
-        }
-    }
-})
+  },
+  Detail: {
+    screen: Detail,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
 const Switch = createSwitchNavigator({
-    AuthStack: AuthStack,
-    App: Route
-})
+  AuthStack: AuthStack,
+  App: Route,
+});
 
-export default createAppContainer(Switch)
+export default createAppContainer(Switch);
--- src/screen/register.js
+++ src/screen/register.js
@@ -23,307 +23,11 @@ import Iconn from 'react-native-vector-icons/SimpleLineIcons';
 
 import {FlatList} from 'react-native-gesture-handler';
 
+import Prov from '../provinsi.json';
+import Kota from '../kota.json';
+
 console.disableYellowBox = true;
 
-const items = [
-  {
-    value: 'Ambon',
-  },
-  {
-    value: 'Balikpapan',
-  },
-  {
-    value: 'Banda Aceh',
-  },
-  {
-    value: 'Bandar Lampung',
-  },
-  {
-    value: 'Bandung',
-  },
-  {
-    value: 'Banjar',
-  },
-  {
-    value: 'Banjarbaru',
-  },
-  {
-    value: 'Banjarmasin',
-  },
-  {
-    value: 'Batam',
-  },
-  {
-    value: 'Batu',
-  },
-  {
-    value: 'Bau-Bau',
-  },
-  {
-    value: 'Bekasi',
-  },
-  {
-    value: 'Bengkulu',
-  },
-  {
-    value: 'Bima',
-  },
-  {
-    value: 'Binjai',
-  },
-  {
-    value: 'Bitung',
-  },
-  {
-    value: 'Blitar',
-  },
-  {
-    value: 'Bogor',
-  },
-  {
-    value: 'Bontang',
-  },
-  {
-    value: 'Bukittinggi',
-  },
-  {
-    value: 'Cilegon',
-  },
-  {
-    value: 'Cimahi',
-  },
-  {
-    value: 'Cirebon',
-  },
-  {
-    value: 'Denpasar',
-  },
-  {
-    value: 'Depok',
-  },
-  {
-    value: 'Dumai',
-  },
-  {
-    value: 'Gorontalo',
-  },
-  {
-    value: 'Jambi',
-  },
-  {
-    value: 'Jayapura',
-  },
-  {
-    value: 'Kediri',
-  },
-  {
-    value: 'Kendari',
-  },
-  {
-    value: 'Jakata Barat',
-  },
-  {
-    value: 'Jakarta Pusat',
-  },
-  {
-    value: 'Jakarta Selatan',
-  },
-  {
-    value: 'Jakarta Timur',
-  },
-  {
-    value: 'Jakarta Utara',
-  },
-  {
-    value: 'Kotamobagu',
-  },
-  {
-    value: 'Kupang',
-  },
-  {
-    value: 'Langsa',
-  },
-  {
-    value: 'Lhokseumawe',
-  },
-  {
-    value: 'Lubuklinggau',
-  },
-  {
-    value: 'Madiun',
-  },
-  {
-    value: 'Magelang',
-  },
-  {
-    value: 'Makassar',
-  },
-  {
-    value: 'Malang',
-  },
-  {
-    value: 'Manado',
-  },
-  {
-    value: 'Mataram',
-  },
-  {
-    value: 'Medan',
-  },
-  {
-    value: 'Metro',
-  },
-  {
-    value: 'Meulaboh',
-  },
-  {
-    value: 'Mojokerto',
-  },
-  {
-    value: 'Padang',
-  },
-  {
-    value: 'Padang Sidempuan',
-  },
-  {
-    value: 'Padangpanjang',
-  },
-  {
-    value: 'Pagaralam',
-  },
-  {
-    value: 'Palangkaraya',
-  },
-  {
-    value: 'Palembang',
-  },
-  {
-    value: 'Palopo',
-  },
-  {
-    value: 'Palu',
-  },
-  {
-    value: 'Pangkalpinang',
-  },
-  {
-    value: 'Parepare',
-  },
-  {
-    value: 'Pariaman',
-  },
-  {
-    value: 'Pasuruan',
-  },
-  {
-    value: 'Payakumbuh',
-  },
-  {
-    value: 'Pekalongan',
-  },
-  {
-    value: 'Pekanbaru',
-  },
-  {
-    value: 'Pematangsiantar',
-  },
-  {
-    value: 'Pontianak',
-  },
-  {
-    value: 'Prabumulih',
-  },
-  {
-    value: 'Probolinggo',
-  },
-  {
-    value: 'Purwokerto',
-  },
-  {
-    value: 'Sabang',
-  },
-  {
-    value: 'Salatiga',
-  },
-  {
-    value: 'Samarinda',
-  },
-  {
-    value: 'Sawahlunto',
-  },
-  {
-    value: 'Semarang',
-  },
-  {
-    value: 'Serang',
-  },
-  {
-    value: 'Sibolga',
-  },
-  {
-    value: 'Singkawang',
-  },
-  {
-    value: 'Solok',
-  },
-  {
-    value: 'Sorong',
-  },
-  {
-    value: 'Subulussalam',
-  },
-  {
-    value: 'Sukabumi',
-  },
-  {
-    value: 'Sungai Penuh',
-  },
-  {
-    value: 'Surabaya',
-  },
-  {
-    value: 'Surakarta',
-  },
-  {
-    value: 'Tangerang',
-  },
-  {
-    value: 'Tangerang Selatan',
-  },
-  {
-    value: 'Tanjungbalai',
-  },
-  {
-    value: 'Tanjungpinang',
-  },
-  {
-    value: 'Tarakan',
-  },
-  {
-    value: 'Tasikmalaya',
-  },
-  {
-    value: 'Tebingtinggi',
-  },
-  {
-    value: 'Tegal',
-  },
-  {
-    value: 'Ternate',
-  },
-  {
-    value: 'Tidore Kepulauan',
-  },
-  {
-    value: 'Tumohon',
-  },
-  {
-    value: 'Tual',
-  },
-  {
-    value: 'Yogyakarta',
-  },
-];
 const JenisKelamin = [
   {
     value: 'Laki - Laki',
@@ -343,53 +47,24 @@ class register extends Component {
       jenis_kelamin: '',
       username: '',
       password: '',
-      error: '',
-      modal: false,
+      modalkota: false,
+      modalprovinsi: false,
       data: [],
+      provinsi: '',
+      id_provinsi: '',
+      kotak: [],
+      provin: [],
+      nama_eror: false,
+      jenis_eror: false,
+      provinsi_eror: false,
+      kota_eror: false,
+      username_eror: false,
+      password_eror: false,
     };
   }
-
-  managePasswordVisibility = () => {
-    this.setState({
-      hidePassword: !this.state.hidePassword,
-    });
-  };
-  toast = () => {
-    ToastAndroid.showWithGravity(
-      'Register berhasil, Silahkan Login',
-      ToastAndroid.LONG,
-      ToastAndroid.CENTER,
-    );
-  };
-  username = () => {
-    ToastAndroid.showWithGravity(
-      'Username sudah terpakai',
-      ToastAndroid.LONG,
-      ToastAndroid.CENTER,
-    );
-  };
-  isi = () => {
-    ToastAndroid.showWithGravity(
-      'isi woy',
-      ToastAndroid.LONG,
-      ToastAndroid.CENTER,
-    );
-  };
-  back = () => {
-    this.toast();
-    this.props.navigation.navigate('Login');
-  };
-  error = () => {
-    const white = '#fff';
-    const red = 'red';
-    if (this.state.error === 'error') {
-      return red;
-    } else {
-      return white;
-    }
-  };
   Register = () => {
     const {nama_lengkap, kota, jenis_kelamin, username, password} = this.state;
+
     return fetch('http://labelmuslim.sipondok.com/api/register', {
       method: 'POST',
       headers: {
@@ -406,39 +81,95 @@ class register extends Component {
     })
       .then(response => response.json())
       .then(response => {
-        if (
-          this.state.nama_lengkap === '' ||
-          this.state.jenis_kelamin === '' ||
-          this.state.kota === ''
-        ) {
-          return this.setState({error: 'error'});
-        } else {
+        console.log(response);
+        if (response == 'Berhasil') {
           console.log(response);
-          if (response == 'Berhasil') {
-            console.log(response);
-            this.back();
-          } else if (response.username) {
-            console.log(response.username);
-            this.username();
-          } else if (response.jenis_kelamin) {
-            console.log(response.jenis_kelamin);
-          } else if (response.nama_lengkap) {
-            console.log(response.nama_lengkap);
-          } else if (response.kota) {
-            console.log(response.kota);
-          }
+          this.back();
+        } else if (response.username) {
+          console.log(response.username);
+          this.username();
         }
       })
       .catch(error => {
         console.log(error);
         this.setState({error: 'error'});
-        alert('error');
       });
   };
   componentDidMount() {
-    this.setState({data: items});
+    this.setState({provin: Prov});
   }
-  search = ({item}) => {
+  managePasswordVisibility = () => {
+    this.setState({
+      hidePassword: !this.state.hidePassword,
+    });
+  };
+  toast = () => {
+    ToastAndroid.showWithGravity(
+      'Register berhasil, Silahkan Login',
+      ToastAndroid.LONG,
+      ToastAndroid.CENTER,
+    );
+  };
+  username = () => {
+    this.setState({username_eror: true});
+    ToastAndroid.showWithGravity(
+      'Username sudah terpakai',
+      ToastAndroid.LONG,
+      ToastAndroid.CENTER,
+    );
+  };
+  usernamenull = () => {
+    ToastAndroid.showWithGravity(
+      'username null',
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER,
+    );
+  };
+  passwordnull = () => {
+    ToastAndroid.showWithGravity(
+      'password null',
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER,
+    );
+  };
+  back = () => {
+    this.toast();
+    this.props.navigation.navigate('Login');
+  };
+  filterkota = item => {
+    this.setState({id_provinsi: item.id});
+    var obj = [];
+    console.log(item);
+    Kota.filter(value => {
+      if (parseInt(value.id_provinsi) === parseInt(item.id)) {
+        obj.push(value);
+        this.setState({
+          kotak: obj,
+          provinsi: item.provinsi,
+          modalprovinsi: false,
+        });
+        console.log(obj);
+      }
+    });
+  };
+  searchprovinsi = ({item}) => {
+    return (
+      <View
+        style={{
+          padding: 13,
+          borderBottomWidth: 0.5,
+          borderColor: 'gray',
+          left: 15,
+        }}>
+        <TouchableOpacity onPress={() => this.filterkota(item)}>
+          <Text style={{fontFamily: 'nunito.regular', fontSize: 20}}>
+            {item.provinsi}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  searchkota = ({item}) => {
     return (
       <View
         style={{
@@ -448,29 +179,97 @@ class register extends Component {
           left: 15,
         }}>
         <TouchableOpacity
-          onPress={() => this.setState({kota: item.value, modal: false})}>
+          onPress={() => this.setState({kota: item.kota, modalkota: false})}>
           <Text style={{fontFamily: 'nunito.regular', fontSize: 20}}>
-            {item.value}
+            {item.kota}
           </Text>
         </TouchableOpacity>
       </View>
     );
   };
-  functionSearch = text => {
-    const newData = items.filter(item => {
-      const itemData = item.value.toUpperCase();
+  functionSearchprovinsi = text => {
+    const newData = Prov.filter(item => {
+      const itemData = item.provinsi.toUpperCase();
       const textData = text.toUpperCase();
       return itemData.indexOf(textData) > -1;
     });
-    this.setState({data: newData, text: text});
+    this.setState({provin: newData, text: text});
+  };
+  functionSearchkota = text => {
+    const newData = this.state.kotak.filter(item => {
+      const itemData = item.kota.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({kotak: newData, text: text});
   };
   kota = () => {
     if (this.state.kota != '') {
       return (
-        <Text style={{color: '#fff', bottom: -10, fontSize: 11}}>
-          Kota Tinggal
-        </Text>
+        <Text style={{color: '#fff', bottom: -10, fontSize: 11}}>Kota</Text>
       );
+    }
+  };
+  provinsi = () => {
+    if (this.state.provinsi != '') {
+      return (
+        <Text style={{color: '#fff', bottom: -10, fontSize: 11}}>Provinsi</Text>
+      );
+    }
+  };
+  kota_kosong = () => {
+    if (this.state.kotak != '') {
+      this.setState({modalkota: true});
+    } else {
+      alert('isi provinsi dulu');
+    }
+  };
+  error = () => {
+    if (
+      this.state.nama_lengkap != '' &&
+      this.state.jenis_kelamin != '' &&
+      this.state.provinsi != '' &&
+      this.state.kota != '' &&
+      this.state.username != '' &&
+      this.state.password != ''
+    ) {
+      this.Register();
+    }
+    if (this.state.nama_lengkap === '') {
+      this.setState({nama_eror: true});
+    } else {
+      this.setState({nama_eror: false});
+    }
+    if (this.state.jenis_kelamin === '') {
+      this.setState({jenis_eror: true});
+    } else {
+      this.setState({jenis_eror: false});
+    }
+    if (this.state.provinsi === '') {
+      this.setState({provinsi_eror: true});
+    } else {
+      this.setState({provinsi_eror: false});
+    }
+    if (this.state.kota === '') {
+      this.setState({kota_eror: true});
+    } else {
+      this.setState({kota_eror: false});
+    }
+    if (this.state.username === '') {
+      // this.setState({username_eror: true});
+      setTimeout(() => {
+        this.usernamenull();
+      }, 100);
+    } else {
+      this.setState({username_eror: false});
+    }
+    if (this.state.password === '') {
+      // this.setState({password_eror: true});
+      setTimeout(() => {
+        this.passwordnull();
+      }, 2000);
+    } else {
+      this.setState({password_eror: false});
     }
   };
   render() {
@@ -480,7 +279,45 @@ class register extends Component {
           flex: 1,
           backgroundColor: '#4A86E8',
         }}>
-        <Modal visible={this.state.modal}>
+        <Modal visible={this.state.modalprovinsi}>
+          <View style={{flex: 1, backgroundColor: '#fff'}}>
+            <View
+              style={{
+                height: 60,
+                backgroundColor: '#4A86E8',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: '#fff',
+                  borderRadius: 50,
+                  alignItems: 'center',
+                  height: 45,
+                  width: '95%',
+                }}>
+                <Icon name="search" style={{left: 10}} size={30} />
+                <TextInput
+                  placeholder="Cari Provinsi"
+                  onChangeText={text => this.functionSearchprovinsi(text)}
+                  style={{
+                    left: 10,
+                    fontFamily: 'nunito.semibold',
+                    color: '#000',
+                    fontSize: 20,
+                  }}
+                />
+              </View>
+            </View>
+            <FlatList
+              data={this.state.provin}
+              renderItem={this.searchprovinsi}
+              keyExtractor={item => item.toString()}
+            />
+          </View>
+        </Modal>
+        <Modal visible={this.state.modalkota}>
           <View style={{flex: 1, backgroundColor: '#fff'}}>
             <View
               style={{
@@ -501,7 +338,7 @@ class register extends Component {
                 <Icon name="search" style={{left: 10}} size={30} />
                 <TextInput
                   placeholder="Cari Kota"
-                  onChangeText={text => this.functionSearch(text)}
+                  onChangeText={text => this.functionSearchkota(text)}
                   style={{
                     left: 10,
                     fontFamily: 'nunito.semibold',
@@ -512,8 +349,8 @@ class register extends Component {
               </View>
             </View>
             <FlatList
-              data={this.state.data}
-              renderItem={this.search}
+              data={this.state.kotak}
+              renderItem={this.searchkota}
               keyExtractor={item => item.toString()}
             />
           </View>
@@ -546,7 +383,7 @@ class register extends Component {
                   tintColor="#fff"
                   fontSize={18}
                 />
-                {this.state.error === 'error' ? (
+                {this.state.nama_eror === true ? (
                   <View
                     style={{
                       position: 'absolute',
@@ -597,7 +434,7 @@ class register extends Component {
                   fontSize={18}
                   data={JenisKelamin}
                 />
-                {this.state.error === 'error' ? (
+                {this.state.jenis_eror === true ? (
                   <View
                     style={{
                       position: 'absolute',
@@ -615,6 +452,43 @@ class register extends Component {
                   <View />
                 )}
               </View>
+              <View
+                style={{
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  width: '85%',
+                  borderBottomWidth: 0.3,
+                  borderBottomColor: '#fff',
+                  height: 50,
+                }}>
+                <TouchableOpacity
+                  onPress={() => this.setState({modalprovinsi: true})}>
+                  {this.provinsi()}
+                  <TextInput
+                    placeholder="Provinsi"
+                    placeholderTextColor="#fff"
+                    editable={false}
+                    value={this.state.provinsi}
+                    style={{color: '#fff', fontSize: 18, left: -5}}
+                  />
+                  {this.state.provinsi_eror === true ? (
+                    <View
+                      style={{
+                        position: 'absolute',
+                        width: '100%',
+                        alignItems: 'flex-end',
+                        justifyContent: 'center',
+                        height: '100%',
+                      }}>
+                      <View style={{backgroundColor: '#4A86E8'}}>
+                        <Icon name="error" size={30} color="red" />
+                      </View>
+                    </View>
+                  ) : (
+                    <View />
+                  )}
+                </TouchableOpacity>
+              </View>
 
               <View
                 style={{
@@ -625,16 +499,16 @@ class register extends Component {
                   borderBottomColor: '#fff',
                   height: 50,
                 }}>
-                <TouchableOpacity onPress={() => this.setState({modal: true})}>
+                <TouchableOpacity onPress={() => this.kota_kosong()}>
                   {this.kota()}
                   <TextInput
-                    placeholder="kota Tinggal"
+                    placeholder="kota "
                     placeholderTextColor="#fff"
                     editable={false}
                     value={this.state.kota}
                     style={{color: '#fff', fontSize: 18, left: -5}}
                   />
-                  {this.state.error === 'error' ? (
+                  {this.state.kota_eror === true ? (
                     <View
                       style={{
                         position: 'absolute',
@@ -707,7 +581,7 @@ class register extends Component {
             </View>
           </ImageBackground>
 
-          <TouchableWithoutFeedback onPress={this.Register}>
+          <TouchableWithoutFeedback onPress={this.error}>
             <View style={styles.viewDaftar}>
               <Text style={styles.textDaftar}>Daftar Sekarang</Text>
             </View>
